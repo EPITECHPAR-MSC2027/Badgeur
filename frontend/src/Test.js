617247573c30854d3ea14c71b9d3a14563e69758
belli <bellinna.uong@outlook.com>
2025-10-07 17:11:47 +0200

    Add variable.css

---
+++ frontend/src/Test.js
@@ -0,0 +1,25 @@
+import './style/theme.css'
+import './Test.css'
+
+function Test() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1> Je suis H1 BADGER</h1>
+                <div>
+                    <h2> Je suis H2</h2>
+                    <p>Je suis text totalement normal.</p>
+                    <p> Je suis un sous-texte</p>
+                </div>
+                <div>
+                    <h2> Je suis H2</h2>
+                    <p>Je suis text totalement normal.</p>
+                    <p> Je suis un sous-texte</p>
+                </div>
+
+            </header>
+        </div>
+    );
+}
+
+export default Test;
