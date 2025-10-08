f40a673bcf79e504a1a34f1587c3e342709a7ba0
belli <bellinna.uong@outlook.com>
2025-10-08 09:16:19 +0200

    Clean file

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,20 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-//import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './style/theme.css'
-import Test from './Test'
-import './Test.css'
+import ParamTre from './pages/Paramètre'
+import './style/Test.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Test />
+    <ParamTre />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
