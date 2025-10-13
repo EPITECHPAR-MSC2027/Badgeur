681c95da1b2a6d43d54f9ed0cfbbd947fa19ee17
belli <bellinna.uong@outlook.com>
2025-10-13 09:59:20 +0200

    Fix buging of not compile with no css

--- frontend/src/pages/Pointage.js
+++ frontend/src/pages/Pointage.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import authService from '../services/authService'
 import '../style/pointage.css'
 
